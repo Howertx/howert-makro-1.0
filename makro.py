1a16ec0036cdfd010790e4a0b70a2719aec8aec3
Howertx <101646261+Howertx@users.noreply.github.com>
2022-05-25 19:56:49 +0300

    Update makro.py

--- makro.py
+++ makro.py
@@ -6,7 +6,7 @@ print("[------------------------]\n     Howert Makro v1.0\n[--------------------
 a = input("Makroyu başlatmak için rastgele bir tuşa tıklayınız. ")
 bb = int(input("Tek tıkta kaç kez basılacağınızı giriniz (Kısaca kaç cps olsun. Craftrise vb. oynayacak iseniz 3 ideal.) >> "))
 gg = input("Makroyu hangi tuşa atayayım? >> ")
-print("Makro başlatılmıştır.")
+print(f"Makro başlatılmıştır. {gg} tuşuna basarak {bb} cps makroyu kullanabilirsiniz.")
 def makro():
     for x in range(bb):
       pyautogui.click()
