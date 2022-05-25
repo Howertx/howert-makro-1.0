7e3c30c4a6a759e0a3e551ad5d0e0bc095efd8ef
Howertx <101646261+Howertx@users.noreply.github.com>
2022-05-25 19:25:11 +0300

    Create makro.py

---
+++ makro.py
@@ -0,0 +1,15 @@
+import keyboard
+import time
+import pyautogui
+
+print("[------------------------]\n     Howert Makro v1.0\n[------------------------]")
+a = input("Makroyu başlatmak için rastgele bir tuşa tıklayınız. ")
+bb = int(input("Tek tıkta kaç kez basılacağınızı giriniz (Kısaca kaç cps olsun. Craftrise vb. oynayacak iseniz 3 ideal.) >> "))
+gg = input("Makroyu hangi tuşa atayayım? >> ")
+print("Makro başlatılmıştır.")
+def makro():
+    for x in range(bb):
+      pyautogui.click()
+while True:
+  if keyboard.is_pressed(f"{gg}"):
+    makro()
